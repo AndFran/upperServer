bb4f020d94a9e3280368dc85a43cbd86e5ae2912
AndFran <andrewwilliamfranks@gmail.com>
2023-12-02 10:39:00 +0100

    Create main.go

---
+++ server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		clientReader := bufio.NewReader(conn) // read from client
+		clientText, err := clientReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = conn.Write([]byte(strings.ToUpper(clientText) + "\n"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Welcome to UPPER server....waiting for a connection")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		fmt.Println("Accepted connection")
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
